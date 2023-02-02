e9258a118e4c8e63e1820725ae41c1ceb7826109
bregor77 <bregor77@gmail.com>
2023-02-02 14:26:59 +0000

    first commit of file script.sh

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir fol_1
+echo "fol_1 created"
+touch fol_1/1_1.txt
+echo "file 1_1.txt created"
+touch fol_1/1_2.txt
+echo "file 1_2.txt created"
+touch fol_1/1_3.txt
+echo "file 1_3.txt created"
+
+mkdir fol_2
+echo "fol_2 created"
+touch fol_2/2_1.txt
+echo "file 2_1.txt created"
+touch fol_2/2_2.txt
+echo "file 2_2.txt created"
+touch fol_2/2_3.txt
+echo "file 2_3.txt created"
